66a05451a69d9430a1ba074aa0314cb1b468ceb8
ndgoodman <52294719+ndgoodman@users.noreply.github.com>
2020-11-17 23:07:07 -0700

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check git for updates
+echo "Checking Opensphere yarn repo for updates"
+cd /workspace/opensphere-yarn-workspace
+git pull
+echo "Checking Opensphere repo for updates"
+cd /workspace/opensphere-yarn-workspace/workspace/opensphere/
+git pull
+
+# Updating yarn dependencies
+echo "Updating yarn dependencies"
+cd /workspace/opensphere-yarn-workspace
+yarn install
+
+# Build Opensphere
+echo "Building OpenSphere"
+cd /workspace/opensphere-yarn-workspace/workspace/opensphere/
+yarn build
+
+# Move Opensphere to web-content
+echo "Move Opensphere to web-content"
+mv cd /workspace/opensphere-yarn-workspace/workspace/opensphere/dist/openshere /web-content/opensphere
